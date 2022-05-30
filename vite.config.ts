1c33a0a3b940cbe20ef0defa05612aa29675ee33
Matthias Haid <matthias@villa-marianne.com>
2022-05-30 17:46:25 +0200

    move files & update config & add imageprocessor

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import htmlPurge from 'vite-plugin-html-purgecss'
+import htmlImages from 'vite-plugin-html-images'
 
 export default {
+    root: "src",
     plugins: [
         htmlPurge({
             content: ["*.html"],
             // whitelist: ["my-very-special-class"],
+        }),
+        htmlImages({
+            tempDirname: '.tempimages',
+            jpeg: {quality: 20, mozjpeg: true}
         }),
     ]
 }
