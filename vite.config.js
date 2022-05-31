17b86e31f0f572860e118f3a938b5a49ff39a867
Matthias Haid <matthias@villa-marianne.com>
2022-05-31 17:38:11 +0200

    add new elements & new page

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default {
             input: {
                 main: resolve(__dirname, 'src/index.html'),
                 thanks: resolve(__dirname, 'src/thanks.html'),
+                more_elements: resolve(__dirname, 'src/more-elements.html'),
                 not_found: resolve(__dirname, 'src/404.html'),
             }
         }
